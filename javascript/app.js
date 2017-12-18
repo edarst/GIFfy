15b44248dd41b27c025580ba00464a1aae8a9b12
Evan Darst <evan.l.darst@gmail.com>
2017-12-17 21:19:30 -0500

    made the button creation function

--- javascript/app.js
+++ javascript/app.js
@@ -4,5 +4,39 @@ var food = ["cheeseburger", "steak", "sandwich", "taco", "burrito", "pizza", "ba
 
 //functions that will render buttons on the page
 
+	function makeButtons () {
+
+//remove any child element with .empty
+
+		$(".buttons-view").empty();
+
+		//loop through the array of food
+
+		for (var i = 0; i < food.length; i++) {
+
+			//make the variable create a button
+
+			var buttonCreate = $("<button>");
+
+			//add the class to the button
+
+			buttonCreate.addClass("food btn btn-default");
+
+			//attribute data-name to the food array and the buttonCreate function
+
+			buttonCreate.attr("data-name", food[i]);
+
+			//pass through the array text to the button
+
+			buttonCreate.text(food[i]);
+
+			//append the buttons view class to the button creation function
+
+			$(".buttons-view").append(buttonCreate);
+
+		} 
+
+
+	}
 
 //run all JS functions
