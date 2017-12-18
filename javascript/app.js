3b122c7bd20d9711b02185e5a105d9abefcf6a6a
Evan Darst <evan.l.darst@gmail.com>
2017-12-17 21:03:32 -0500

    added variables

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,7 @@
 // set variables
 
+var food = ["cheeseburger", "steak", "sandwich", "taco", "burrito", "pizza", "bacon", "sausage", "cheesecake", "pie", "ice cream"];
+
 //functions that will render buttons on the page
 
 
